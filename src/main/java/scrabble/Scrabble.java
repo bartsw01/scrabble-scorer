bbbd0e17c2465d3a4fcce713460f1d9fb4b2ff3b
Beth Hansen and Bart Webber <bart@>
2017-08-01 14:18:47 -0700

    add values for the different scrabble letters

--- src/main/java/scrabble/Scrabble.java
+++ src/main/java/scrabble/Scrabble.java
@@ -5,22 +5,34 @@ import java.util.Map;
 import java.util.HashMap;
 
 
+
 /**
  * Created by Guest on 8/1/17.
  */
 public class Scrabble {
+    public Integer scrabbleScore = 0;
 
-    public Integer calculateScore(String letter) {
-        Integer result = 0;
-        if (letter.equals("a") || letter.equals("e") || letter.equals("i") || (letter.equals("o") || letter.equals("u") || letter.equals("l") || (letter.equals("n") || letter.equals("r") || letter.equals("s") || letter.equals("t") ))) {
-            result += 1;
-        } else if(letter.equals("aa")) {
-            result += 2;
-        } else if(letter.equals("do")){
-            result += 3;
+    public Integer calculateScore(String  word) {
+        String lowerCaseWord = word.toLowerCase();
+        String[] wordArray = lowerCaseWord.split(" ");
+        for (String individualLetter : wordArray) {
+            if (individualLetter.equals("a") || individualLetter.equals("e") || individualLetter.equals("i") || individualLetter.equals("o") || individualLetter.equals("u") || individualLetter.equals("l") || individualLetter.equals("n") || individualLetter.equals("r") || individualLetter.equals("s") || individualLetter.equals("t")) {
+                this.scrabbleScore += 1;
+            } else if (individualLetter.equals("d") || individualLetter.equals("g")) {
+                this.scrabbleScore += 2;
+            } else if (individualLetter.equals("b") || individualLetter.equals("c") || individualLetter.equals("m") || individualLetter.equals("p")) {
+                this.scrabbleScore += 3;
+            } else if (individualLetter.equals("f") || individualLetter.equals("h") || individualLetter.equals("v") || individualLetter.equals("w") || individualLetter.equals("y")){
+                this.scrabbleScore += 4;
+            } else if (individualLetter.equals("k")) {
+                this.scrabbleScore += 5;
+            } else if (individualLetter.equals("j") || individualLetter.equals("x")){
+                this.scrabbleScore += 8;
+            } else if (individualLetter.equals("q") || individualLetter.equals("z")){
+                this.scrabbleScore += 10;
 
-        }
-        return result;
+            }
+        }   return this.scrabbleScore;
     }
 }
 
